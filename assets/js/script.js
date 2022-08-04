6616dc5421afd7edd0ebce2ab923d1407f4e594f
Zuyel Rana <zuyel76@gmail.com>
2022-08-04 22:14:20 +0600

    Education website ui 2

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// navbar variables
+const navbarNav = document.querySelector('.navbar-nav');
+const navbarToggleBtn = document.querySelector('.nav-toggle-btn');
+
+// navbar toggle functionality
+navbarToggleBtn.addEventListener('click', function () {
+
+  navbarNav.classList.toggle('active');
+  this.classList.toggle('active');
+
+});
+
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 100) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+}
+// Swiper js
+
+
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+  effect: "creative",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+});
